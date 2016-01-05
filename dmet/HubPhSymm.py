f0688fc00976692b777a9d927d1773ce5f8b5361
Bo-Xiao Zheng <boxiao.zheng@gmail.com>
2016-01-04 19:26:53 -0500

    allow restricting range of vcor

--- dmet/HubPhSymm.py
+++ dmet/HubPhSymm.py
@@ -98,18 +98,21 @@ def transformResults(rhoEmb, E, basis, ImpHam, H1e):
 
     return rhoImp, Efrag/nscsites, nelec/nscsites
 
-def InitGuess(ImpSize, U, polar = None):
+def InitGuess(ImpSize, U, polar = None, r = None):
     subA, subB = BipartiteSquare(ImpSize)
-    v = VcorLocalPhSymm(U, False, subA, subB)
-    nscsites = len(subA) + len(subB)
+    v = VcorLocalPhSymm(U, False, ImpSize, subA, subB, r)
+
     if polar is None:
         polar = U/2
+
+    nscsites = np.product(ImpSize)
     init_v = np.eye(nscsites) * U/2
     init_p = np.diag(map(lambda s: polar if s in subA else -polar, range(nscsites)))
     v.assign(np.asarray([init_v + init_p, init_v - init_p]))
+
     return v
 
-def VcorLocalPhSymm(U, bogoliubov, subA, subB):
+def VcorLocalPhSymm(U, bogoliubov, ImpSize, subA, subB, r = None):
     # with particle-hole symmetry, on two sublattices
     # specifically for t'=0 Hubbard model at half-filling
     # unrestricted potential is assumed
@@ -117,30 +120,42 @@ def VcorLocalPhSymm(U, bogoliubov, subA, subB):
     # VA_{ij} + (-)^{i+j}VB_{ij} = 0
     # D_{ij} = (-)^{i+j}D_{ji}
     # AA=+, AB=-, BB=+
+    assert(np.asarray(ImpSize).shape == (2,))
     subA, subB = set(subA), set(subB)
     log.eassert(len(subA) == len(subB), "number of sites in two sublattices are equal")
     nscsites = len(subA) * 2
     log.eassert(subA | subB == set(range(nscsites)), "sublattice designation problematic")
-    nV = nscsites * (nscsites+1) / 2
+    nscsites = np.product(ImpSize)
+    log.eassert(subA | subB == set(range(nscsites)), "sublattice designation problematic")
+
+    if r is None:
+        pairs = list(it.combinations_with_replacement(range(nscsites), 2))
+    else:
+        pairs = []
+        sites = list(enumerate(it.product(*map(range, ImpSize))))
+        for (i, ri), (j, rj) in it.combinations_with_replacement(sites, 2):
+            if la.norm(np.asarray(ri) - np.asarray(rj)) < r+1e-6:
+                pairs.append((i, j))
+
+    nV = len(pairs)
 
     v = vcor.Vcor()
     v.grad = None
 
-    def sign(i,j):
+    def sign(i, j):
         if (i in subA) == (j in subA):
             return 1
         else:
-            return -1
+            return 0
 
     if bogoliubov:
-        nD = nscsites * (nscsites+1) / 2
+        nD = nV
         def evaluate(self):
             log.eassert(self.param.shape == (nV+nD,), "wrong parameter shape, require %s", (nV+nD,))
             V = np.zeros((3, nscsites, nscsites))
-            for idx, (i,j) in enumerate(it.combinations_with_replacement(range(nscsites), 2)):
+            for idx, (i, j) in enumerate(pairs):
                 V[0,i,j] = V[0,j,i] = self.param[idx]
                 V[1,i,j] = V[1,j,i] = -self.param[idx] * sign(i,j)
-            for idx, (i,j) in enumerate(it.combinations_with_replacement(range(nscsites), 2)):
                 V[2,i,j] = self.param[idx+nV]
                 if i != j:
                     V[2,j,i] = self.param[idx+nV] * sign(i,j)
@@ -151,10 +166,9 @@ def VcorLocalPhSymm(U, bogoliubov, subA, subB):
         def gradient(self):
             if self.grad is None:
                 g = np.zeros((nV+nD, 3, nscsites, nscsites))
-                for idx, (i,j) in enumerate(it.combinations_with_replacement(range(nscsites), 2)):
+                for idx, (i,j) in enumerate(pairs):
                     g[idx,0,i,j] = g[idx,0,j,i] = 1
                     g[idx,1,i,j] = g[idx,1,j,i] = -sign(i,j)
-                for idx, (i,j) in enumerate(it.combinations_with_replacement(range(nscsites), 2)):
                     g[idx+nV,2,i,j] = 1
                     if i != j:
                         g[idx+nV,2,j,i] = sign(i,j)
@@ -165,7 +179,7 @@ def VcorLocalPhSymm(U, bogoliubov, subA, subB):
         def evaluate(self):
             log.eassert(self.param.shape == (nV,), "wrong parameter shape, require %s", (nV,))
             V = np.zeros((2, nscsites, nscsites))
-            for idx, (i,j) in enumerate(it.combinations_with_replacement(range(nscsites), 2)):
+            for idx, (i,j) in enumerate(pairs):
                 V[0,i,j] = V[0,j,i] = self.param[idx]
                 V[1,i,j] = V[1,j,i] = -self.param[idx] * sign(i,j)
             V[0] += np.eye(nscsites) * (U/2)
@@ -175,7 +189,7 @@ def VcorLocalPhSymm(U, bogoliubov, subA, subB):
         def gradient(self):
             if self.grad is None:
                 g = np.zeros((nV, 2, nscsites, nscsites))
-                for idx, (i,j) in enumerate(it.combinations_with_replacement(range(nscsites), 2)):
+                for idx, (i,j) in enumerate(pairs):
                     g[idx,0,i,j] = g[idx,0,j,i] = 1
                     g[idx,1,i,j] = g[idx,1,j,i] = -sign(i,j)
                 self.grad = g
@@ -187,6 +201,7 @@ def VcorLocalPhSymm(U, bogoliubov, subA, subB):
     return v
 
 def FitVcor(rho, lattice, basis, vcor, beta, MaxIter1 = 300, MaxIter2 = 20):
+    log.info("degrees of freedom = %d", vcor.length())
     return slater.FitVcorTwoStep(rho, lattice, basis, vcor, beta, 0.5, \
             MaxIter1, MaxIter2)
 
